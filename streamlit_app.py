029c92dd5bd86a830eb73aa6dcd6948330bd2468
anagorodisch <ana.gorodisch.embryoxite@gmail.com>
2024-10-07 13:31:21 +0000

    prediccion

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
+import xgboost as xgb
+
+model = xgb.Booster()
+model.load_model('/workspaces/ploidy-prediction/necessary_files/checkpoints.json')
 
 # Título de la aplicación
 st.title("PREDICCIÓN DE PLOIDÍA")
 
-edad_ovocitos = st.slider("Edad Ovocitos", 0, 100)
+edad_ovocitos = st.slider("Edad Ovocitos", 15, 80)
 
 # Crear dos columnas
 col1, col2, col3 = st.columns(3)
@@ -37,4 +41,22 @@ variables = {
 }
 
 df = pd.DataFrame([variables])
-df
+#df
+
+columnas_categoricas = ['PROCEDENCIA OVOCITOS',"PROCEDENCIA SEMEN","TROFODERMO","DESTINO","MCI"]
+df[columnas_categoricas] = df[columnas_categoricas].astype('category')
+columnas_int = ["EDAD PTE OVOCITOS","DIA EMBRION","GRADO EXPANSIÓN"]
+df[columnas_int] = df[columnas_int].astype('int')
+
+if st.button("Predecir"):
+
+    dmatrix = xgb.DMatrix(df, enable_categorical=True)
+    prediction_probs = model.predict(dmatrix)
+
+    predicted_class = (prediction_probs > 0.5).astype(int).flatten()
+    st.subheader("Resultado de la Predicción:")
+
+    prediction_text = "Euploide" if predicted_class == 1 else "Aneuploide"
+
+    probabilidad = prediction_probs[0] if predicted_class == 1 else 1 - prediction_probs[0]
+    st.write(f"Hay un {probabilidad*100:.2f}% de probabilidad de que el embrión sea {prediction_text}")
